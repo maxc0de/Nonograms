a3fb128cefe4515c9021c1116c6af5dbe11fe18d
Максим Комиссаров <kmy.90@yandex.ru>
2019-10-30 12:55:34 +0300

    Добавьте файлы проекта.

---
+++ Nonograms/Cell.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Nonograms
+{
+    class Cell : TextBox
+    {
+        bool marked;
+        bool skipped;
+        public bool Marked
+        {
+            get
+            {
+                return marked;
+            }
+            set
+            {
+                marked = value;
+
+                if (value)
+                {
+                    Skipped = false;
+                    Background = new SolidColorBrush(Colors.Black);
+                }
+                else
+                {
+                    Background = new SolidColorBrush(Colors.White);
+                }
+            }
+        }
+        public bool Skipped
+        {
+            get
+            {
+                return skipped;
+            }
+            set
+            {
+                skipped = value;
+
+                if (value)
+                {
+                    Marked = false;
+                    Background = new SolidColorBrush(Colors.LightGray);
+                }
+                else
+                {
+                    Background = new SolidColorBrush(Colors.White);
+                }
+            }
+        }
+        public Cell()
+        {
+            this.Height = 20;
+            this.Width = 20;
+            this.PreviewMouseDown += Cell_MouseDown;
+            this.PreviewMouseDown += MainWindow.CheckEquals;
+            this.Cursor = Cursors.Arrow;
+            this.IsReadOnly = true;
+        }
+        public Cell(bool isPicCell) : this()
+        {
+            if (isPicCell)
+            {
+                Marked = true;
+            }
+            else
+            {
+                Background = new SolidColorBrush(Colors.Gray);
+                IsEnabled = false;
+            }
+        }
+
+        private void Cell_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                (sender as Cell).Marked = !(sender as Cell).Marked;
+            }
+
+            if (e.RightButton == MouseButtonState.Pressed)
+            {
+                (sender as Cell).Skipped = !(sender as Cell).Skipped;
+            }
+
+        }
+    }
+}
---
+++ Nonograms/GameField.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Nonograms
+{
+    class GameField : UniformGrid
+    {
+        private int size;
+        public Cell[,] cells;
+        List<string[]> nonogram;
+
+        public List<List<int>> rowsNums = new List<List<int>>();
+        public List<List<int>> columnsNums = new List<List<int>>();
+        private int widthRowsNums;
+        private int heightColumnsNums;
+
+        public GameField() { }
+        public GameField(int size)
+        {
+            this.size = size;
+        }
+        public GameField(List<string[]> nonogram)
+        {
+            this.nonogram = nonogram;
+            this.size = nonogram[0].Count();
+
+            cells = new Cell[size,size];
+
+            СalculationNums();
+
+            //Отрисовка
+            this.Rows = this.Columns = size;
+
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    if (nonogram[i][j] == "1")
+                    {
+                        Cell cell = new Cell(true);
+                        this.Children.Add(cell);
+                        cells[i, j] = cell;
+                    }
+                    else
+                    {
+                        Cell cell = new Cell();
+                        this.Children.Add(cell);
+                        cells[i, j] = cell;
+                    }
+                }
+            }
+        }
+
+        public GameField GetGameFieldColumns()
+        {
+            GameField field = new GameField();
+
+            field.Rows = size;
+            field.Columns = heightColumnsNums;
+
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < heightColumnsNums; j++)
+                {
+                    try
+                    {
+                        field.Children.Add(new Cell(false)
+                        {
+                            Text = $"{rowsNums[i][j]}",
+
+                        });
+                    }
+                    catch
+                    {
+                        field.Children.Add(new Cell(false)
+                        {
+                            Text = "",
+                        });
+                    }
+
+                }
+            }
+
+
+            return field;
+        }
+        public GameField GetGameFieldRows()
+        {
+            GameField field = new GameField();
+
+            field.Rows = widthRowsNums;
+            field.Columns = size;
+
+            for (int i = 0; i < widthRowsNums; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    try
+                    {
+                        field.Children.Add(new Cell(false)
+                        {
+                            Text = $"{columnsNums[j][i]}",
+                    });
+                    }
+                    catch
+                    {
+                        field.Children.Add(new Cell(false)
+                        {
+                            Text = "",
+                    });
+                    }
+
+                }
+            }
+
+            return field;
+        }
+        public GameField GetEmpty()
+        {
+            GameField field = new GameField();
+            field.cells = new Cell[size, size];
+
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    Cell cell = new Cell();
+                    field.Children.Add(cell);
+                    field.cells[i, j] = cell;
+                }
+            }
+
+            return field;
+        }
+        private void СalculationNums()
+        {
+            int n1 = 0;
+            int n2 = 0;
+            List<int> m1 = new List<int>();
+            List<int> m2 = new List<int>();
+
+            //Вычисление клеток
+            for (int i = 0; i < size; i++)
+            {
+                n1 = 0;
+                n2 = 0;
+                m1 = new List<int>();
+                m2 = new List<int>();
+
+                for (int j = 0; j < size; j++)
+                {
+
+                    if (nonogram[i][j] == "1")
+                    {
+                        n1++;
+                    }
+                    else if (n1 != 0)
+                    {
+                        m1.Add(n1);
+                        n1 = 0;
+                    }
+
+
+                    if (nonogram[j][i] == "1")
+                    {
+                        n2++;
+                    }
+                    else if (n2 != 0)
+                    {
+                        m2.Add(n2);
+                        n2 = 0;
+                    }
+                }
+
+                if (n1 != 0)
+                {
+                    m1.Add(n1);
+                }
+
+                if (n2 != 0)
+                {
+                    m2.Add(n2);
+                }
+
+                rowsNums.Add(m1);
+                columnsNums.Add(m2);
+
+                widthRowsNums = m1.Count > widthRowsNums ? m1.Count : widthRowsNums;
+                heightColumnsNums = m2.Count > heightColumnsNums ? m2.Count : heightColumnsNums;
+            }
+        }
+        public bool Equals(GameField game)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    if (cells[i,j].Marked != game.cells[i,j].Marked)
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+    }
+}
